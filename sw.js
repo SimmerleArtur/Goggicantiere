0c1050a96f4e496aaffc60e3a29659ae135ba8c4
SimmerleArtur <simmerle.joachim@gmail.com>
2026-05-14 10:42:17 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE = 'goggicantiere-v6-category-blackfix';
+const ASSETS = ['./','./index.html','./styles.css','./app.js','./storage.js','./priceLists.js','./articles.json','./manifest.webmanifest','./logo.png','./logo-full.png','./logo-monogram.png'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', e => { if(e.request.method !== 'GET') return; e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => { const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res; }).catch(()=>caches.match('./index.html')))); });
